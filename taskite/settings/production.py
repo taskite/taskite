18aff45f257c806d65828b5397b940d844c193e7
Santosh Kumar Patro <santoshkpatro11@gmail.com>
2024-08-14 11:43:19 +0530

    added allowed host setting from environment variables

--- taskite/settings/production.py
+++ taskite/settings/production.py
@@ -4,7 +4,7 @@ from taskite.settings.base import *
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = False
 
-ALLOWED_HOSTS = ["taskite-7a7818f32166.herokuapp.com"]
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
 STORAGES = {
     # ...
