ded23f8c3f708785b49aaba63070612cd91c2a43
Santosh Kumar Patro <santoshkpatro11@gmail.com>
2024-09-04 01:25:42 +0530

    added few more prod settings

--- taskite/settings/production.py
+++ taskite/settings/production.py
@@ -1,16 +1,21 @@
 import os
+import sentry_sdk
+import dj_database_url
 from taskite.settings.base import *
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = False
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 
 BASE_URL = os.environ.get("BASE_URL", "https://taskite.in")
 APP_URL = os.environ.get("APP_URL", "https://taskite.in")
 
 CELERY_BROKER_URL = os.environ.get("REDIS_URL")
 CELERY_RESULT_BACKEND = os.environ.get("REDIS_URL")
+
+
+DATABASES = {"default": dj_database_url.config(default=os.environ.get("DATABASE_URL"))}
 
 DEFAULT_FROM_EMAIL = os.environ.get(
     "DEFAULT_FROM_EMAIL", "<Taskite no-reply@taskite.in>"
@@ -34,3 +39,12 @@ STORAGES = {
 SCOUT_MONITOR = True
 SCOUT_KEY = os.environ.get("SCOUT_KEY")
 SCOUT_NAME = os.environ.get("SCOUT_NAME", "Taskite")
+
+
+# Sentry Setup
+sentry_sdk.init(
+    dsn=os.environ.get("SENTRY_DSN"),
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for tracing.
+    traces_sample_rate=1.0,
+)
